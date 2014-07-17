f781ef78a799ffef50df6e7b36e187193b3a4722
Patrick Ellis <oneredoak@gmail.com>
2014-07-16 18:43:52 -0700

    Addition of Unit Test and Code Coverage Reporting

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 PHP Sample with Redis
 =================
 
-Sample PHP project using Redis.
+Sample PHP project using Redis with PHPUnit and Xdebug used for unit test and coverage reporting.
--- RedisExample.php
+++ RedisExample.php
@@ -1,11 +1,11 @@
 <?php
-class MongoTest extends PHPUnit_Framework_TestCase {
-  public function test() {
+class RedisExample {
+  public function populate() {
     $redis = new Redis();
 	$redis->connect('127.0.0.1', 6379);
 	if(!$redis->exists('key')) 
 		$redis->set('key', 'val');
-    $this->assertEquals($redis->get('key'), 'val');
+    return $redis->get('key');
   }
 }
 ?>
